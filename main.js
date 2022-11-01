25a38d524f408b349c109e7c50b7b7c132e77301
Jhonny <gonzodzen@mail.ru>
2022-11-01 09:28:49 +0300

    Next commit.

--- main.js
+++ main.js
@@ -1,20 +1,21 @@
 async function getCurrencies() {
-    const url = '//www.cbr-xml-daily.ru/daily_json.js'
-    const response = await fetch(url)
-    const data = await response.json()
+  const url = "//www.cbr-xml-daily.ru/daily_json.js";
+  const response = await fetch(url);
+  const data = await response.json();
 
+  const usd = document.querySelector("#usd");
+  const eur = document.querySelector("#eur");
+  const blr = document.querySelector("#blr");
 
-    const usd = document.querySelector('#usd')
-    const eur = document.querySelector('#eur')
-    const blr = document.querySelector('#blr')
+  const usdElement =
+    data.Valute.USD.Value.toFixed(2) / data.Valute.BYN.Value.toFixed(2);
+  const eurElement =
+    data.Valute.EUR.Value.toFixed(2) / data.Valute.BYN.Value.toFixed(2);
+  const rubElement = data.Valute.BYN.Value.toFixed(2) / 6.15;
 
-    const usdElement = data.Valute.USD.Value.toFixed(2) / data.Valute.BYN.Value.toFixed(2)
-    const eurElement = data.Valute.EUR.Value.toFixed(2) / data.Valute.BYN.Value.toFixed(2)
-    const rubElement = data.Valute.BYN.Value.toFixed(2) / 6.15
-
-    usd.innerText = usdElement.toFixed(2)
-    eur.innerText = eurElement.toFixed(2)
-    blr.innerText = rubElement.toFixed(2)
+  usd.innerText = usdElement.toFixed(2);
+  eur.innerText = eurElement.toFixed(2);
+  blr.innerText = rubElement.toFixed(2);
 }
 
-getCurrencies()
+getCurrencies();
